857e86cb8a58c9c76b31107dd884d6d31ee4cbfa
Luis Felipe <luisfgaravil@gmail.com>
2020-01-03 13:57:41 -0600

    Minor error fixed (casi nada)

--- app/src/main/java/com/example/concertscollector/Feed.java
+++ app/src/main/java/com/example/concertscollector/Feed.java
@@ -12,16 +12,29 @@ import android.view.View;
 
 public class Feed extends AppCompatActivity {
 
+    FloatingActionButton btn_messages,btn_search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-        FloatingActionButton fab = findViewById(R.id.btn_messages);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Relaciones------------------------
+        btn_messages = findViewById(R.id.btn_messages);
+        btn_search = findViewById(R.id.btn_search);
+
+        btn_messages.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Ir a mensajes", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Ir a búsqueda", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
